a48ef9ae8c8314a6ca7e0ec9ff6b47e672bc8f55
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-13 14:35:32 +0530

    things

--- js/App.js
+++ js/App.js
@@ -1,10 +1,34 @@
 import NotesView from './NotesView.js';
+import NotesAPI from './NotesAPI.js';
 
 export default class App {
     constructor(root) {
         this.notes = [];
         this.activeNote = null;
         this.view = new NotesView(root, this._handlers());
+
+        this._refreshNotes();
+    }
+
+    _refreshNotes() {
+        const notes = NotesAPI.getAllNotes();
+
+        this._setNotes(notes);
+
+        if (notes.length > 0) {
+            this._setActiveNote(notes[0])
+        }
+    }
+
+    _setNotes(notes) {
+        this.notes = notes;
+        this.view.updateNoteList(notes);
+        this.view.updateNotePreviewVisibility(notes.length > 0);
+    }
+
+    _setActiveNote(note) {
+        this.activeNote = note;
+        this.view.updateActiveNote(note);
     }
 
     _handlers() {
