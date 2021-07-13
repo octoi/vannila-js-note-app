5ea679ec14d82df93970b10d5bd50ae7bd051102
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-13 13:21:28 +0530

    delete note

--- js/NotesAPI.js
+++ js/NotesAPI.js
@@ -29,6 +29,9 @@ export default class NotesAPI {
     }
 
     static deleteNote(id) {
+        const notes = NotesAPI.getAllNotes();
+        const wantedNotes = notes.filter(note => note.id !== id);
 
+        localStorage.setItem('notes', JSON.stringify(wantedNotes));
     }
 }
