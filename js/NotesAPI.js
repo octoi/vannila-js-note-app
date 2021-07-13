ef3c966086dfb265222b40ff1a1a6aded4263b6a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-13 13:17:00 +0530

    insert new note

--- js/NotesAPI.js
+++ js/NotesAPI.js
@@ -8,7 +8,31 @@ export default class NotesAPI {
     }
 
     static saveNote(noteToSave) {
+        const notes = NotesAPI.getAllNotes();
+        // 1626162247963
+        const existing = notes.find(note => note.id === noteToSave.id);
 
+
+        // if (existing) {
+
+        //     existing.title = noteToSave.title;
+        //     existing.body = noteToSave.body;
+        //     existing.updated = new Date().toISOString();
+
+        // } else {
+
+        //     noteToSave.id = Date.now();
+        //     noteToSave.updated = new Date().toISOString();
+        //     notes.push(noteToSave);
+
+        // }
+
+        noteToSave.id = Date.now();
+        noteToSave.updated = new Date().toISOString();
+        notes.push(noteToSave);
+
+
+        localStorage.setItem('notes', JSON.stringify(notes));
     }
 
     static deleteNote(id) {
