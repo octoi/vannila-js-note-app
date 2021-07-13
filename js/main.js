003cb6507864b4b8558ff7e0f426e745f6c05cb4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-13 13:29:09 +0530

    using thinfgs

---
+++ js/main.js
@@ -0,0 +1,12 @@
+import NotesAPI from './NotesAPI.js';
+import NotesView from './NotesView.js';
+
+const app = document.getElementById('app');
+const view = new NotesView(app, {
+    onNoteSelect: () => {
+
+    },
+    onNoteSelect: () => {
+
+    },
+})
