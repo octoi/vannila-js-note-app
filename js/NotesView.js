20758877ad8398b242ebddba907533504a57113f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-13 13:40:13 +0530

    listener

--- js/NotesView.js
+++ js/NotesView.js
@@ -8,22 +8,31 @@ export default class NotesView {
 
         this.root.innerHTML = `
             <div class="notes-sidebar">
-            <button class="notes-add" type="button">Add Note</button>
-
-                <div class="notes-list">
-                    <div class="notes-list-item notes-list-item--selected">
-                        <div class="notes-small-title">Lecture Notes</div>
-                        <div class="notes-small-body">I Learn nothing today</div>
-                        <div class="notes-small-updated">date</div>
-                    </div>
-                </div>
+                <button class="notes-add" type="button">Add Note</button>
+                <div class="notes-list"></div>
             </div>
-
             <div class="notes-preview">
-                <input type="text" class="notes-title" placeholder="Enter a title ...">
-                <textarea class="notes-body">I'm the notes body</textarea>
+                <input type="text" class="notes-title" value="New Note" placeholder="Enter a title">
+                <textarea placeholder="Take note ..." class="notes-body">Take note ...</textarea>
             </div>
         `;
+
+        const btnAddNote = this.root.querySelector('.notes-add');
+        const inpTitle = this.root.querySelector('.notes-title');
+        const inpBody = this.root.querySelector('.notes-body');
+
+        btnAddNote.addEventListener('click', () => {
+            this.onNoteAdd();
+        });
+
+        [inpTitle, inpBody].forEach(inputField => {
+            inputField.addEventListener('blur', () => {
+                const updatedTitle = inpTitle.value.trim();
+                const updatedBody = inpBody.value;
+
+                this.onNoteEdit(updatedTitle, updatedBody);
+            });
+        })
     }
 
 }
