3b2c36bc4e000165b0975b0180ec6f8929768d53
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-13 14:29:50 +0530

    handlers

--- js/App.js
+++ js/App.js
@@ -4,6 +4,23 @@ export default class App {
     constructor(root) {
         this.notes = [];
         this.activeNote = null;
-        this.view = new NotesView(root, {});
+        this.view = new NotesView(root, this._handlers());
+    }
+
+    _handlers() {
+        return {
+            onNoteSelect: noteId => {
+                alert("a")
+            },
+            onNoteAdd: () => {
+                alert("b")
+            },
+            onNoteEdit: (title, body) => {
+                alert("c")
+            },
+            onNoteDelete: noteId => {
+                alert("d")
+            },
+        };
     }
 }
